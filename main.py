7e463f7815b60f4f7a4008b8abe99205785049f0
Colombo <luca1.babbu@gmail.com>
2026-06-10 09:21:44 +0200

    aggiunta team

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from dotenv import load_dotenv
+from agno.team.team import Team
+from agno.team.mode import TeamMode
 from agno.agent import Agent
 from agno.run.agent import RunOutput
 from agno.db.sqlite import SqliteDb
@@ -10,7 +12,16 @@ import startConvAgent
 import convAgent
 load_dotenv()
 
+team = Team(
+    name="PragmAI",
+    mode=TeamMode.route,
+    model=Gemini(id="gemini-2.5-flash-lite"),
+    members=[convAgent.convAgent, startConvAgent.startAgent],
+    add_history_to_context=True,
+    db=SqliteDb(db_file="tmp/team.db"),
+    instructions="Prima esegui startAgent, poi quando viene richiamato il tool di fine dialogo richiama convAgent"
+)
 
 # Parte necessaria per permettere all'agente di essere eseguito su AgnoOS
-agent_os = AgentOS(agents=[convAgent.convAgent, startConvAgent.startAgent])
+agent_os = AgentOS(agents=[convAgent.convAgent, startConvAgent.startAgent], teams=[team])
 app = agent_os.get_app()
